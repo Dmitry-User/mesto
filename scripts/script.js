0fec74e00c177b6e670772d682d9f2aaf07f93fe
Dmitry Pavlov <dimapav1985@mail.ru>
2022-05-18 18:35:17 +0700

    feat: added script.js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const buttonEdit = document.querySelector('.profile__button-edit');
+const buttondd = document.querySelector('.profile__button-add');
+const buttonClose = document.querySelector('.popup__button-close');
+const buttonSave = document.querySelector('.popup__button-save');
+
+
+
+// console.log(buttonSave)
