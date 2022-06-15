acd4466653260ab28ef33bbea60b1e5625217bd3
Dmitry Pavlov <dimapav1985@mail.ru>
2022-06-15 19:57:31 +0700

    feat: I can validation inputs

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,100 +1,62 @@
-// const formElement = document.querySelector('.popup__form');
-// const formInput = formElement.querySelector('.popup__input');
-// const formError =formElement.querySelector(`.${formInput.id}-error`);
-
-// console.log(formError);
-
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, dataElements) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_error');
+  inputElement.classList.add(dataElements.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__error_visible');
+  errorElement.classList.add(dataElements.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, dataElements) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_error');
-  errorElement.classList.remove('popup__error_visible');
+  inputElement.classList.remove(dataElements.inputErrorClass);
+  errorElement.classList.remove(dataElements.errorClass);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, dataElements) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, dataElements);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, dataElements);
   }
 };
 
+const setEventListeners = (formElement, dataElements) => {
+  const inputList = Array.from(formElement.querySelectorAll(dataElements.inputSelector));
+  const buttonElement = formElement.querySelector(dataElements.submitButtonSelector);
 
-const setEventListeners = (formElement) => {
-  // Находим все поля внутри формы,
-  // сделаем из них массив методом Array.from
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-   // Найдём в текущей форме кнопку отправки
-  const buttonElement = formElement.querySelector('.popup__submit');
-
-  toggleButtonState(inputList, buttonElement);
-
-  // Обойдём все элементы полученной коллекции
-  inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
-    inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid,
-      // передав ей форму и проверяемый элемент
-      checkInputValidity(formElement, inputElement);
-
-      toggleButtonState(inputList, buttonElement);
+  toggleButtonState(inputList, buttonElement, dataElements);
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement, dataElements);
+      toggleButtonState(inputList, buttonElement, dataElements);
     });
   });
 };
 
-
-const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
-  return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся функция
-    // hasInvalidInput вернёт true
-
-    return !inputElement.validity.valid;
-  })
-}; 
-
-// Функция принимает массив полей ввода
-// и элемент кнопки, состояние которой нужно менять
-
-const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.classList.add('popup__submit_disable');
-  } else {
-    // иначе сделай кнопку активной
-    buttonElement.classList.remove('popup__submit_disable');
-  }
-}; 
-
-
-const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-
-  // Переберём полученную коллекцию
-  formList.forEach((formElement) => {
-    // formElement.addEventListener('submit', (evt) => {
-    //   У каждой формы отменим стандартное поведение
-    //   evt.preventDefault();
-    // });
-
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    setEventListeners(formElement);
-  });
+const enableValidation = dataElements => {
+  const formList = Array.from(document.querySelectorAll(dataElements.formSelector));
+  formList.forEach(formElement => setEventListeners(formElement, dataElements));
 };
 
-// Вызовем функцию
-enableValidation();
+const hasInvalidInput = inputList => {
+  return inputList.some(inputElement => {
+    return !inputElement.validity.valid;
+  }); 
+};
 
+const toggleButtonState = (inputList, buttonElement, dataElements) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(dataElements.inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(dataElements.inactiveButtonClass);
+  }
+};
 
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_disable',
+  inputErrorClass: 'popup__input_error',
+  errorClass: 'popup__error_visible'
+});
