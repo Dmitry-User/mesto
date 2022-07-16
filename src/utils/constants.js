d47c37d79f9e24348bccbb7a6103816ab9aa8bcc
Dmitry Pavlov <dimapav1985@mail.ru>
2022-07-16 22:38:12 +0700

    feat: add new structure files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -25,7 +25,7 @@ const initialCards = [
   }
 ];
 
-const settingsValidation = {
+export const settingsValidation = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit',
   inactiveButtonClass: 'popup__submit_disable',
@@ -33,4 +33,3 @@ const settingsValidation = {
   errorClass: 'popup__error_visible'
 };
 
-export { initialCards, settingsValidation };
