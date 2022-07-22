0ae1ff0646e5507cedaba64e869d6eea07b344e8
Dmitry Pavlov <dimapav1985@mail.ru>
2022-07-22 19:40:48 +0700

    fix: bugfix handleEscape

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,6 +2,7 @@ export default class Popup {
 
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(evt) {
@@ -12,16 +13,12 @@ export default class Popup {
 
   open() {
     this._popup.classList.add('popup_is-open');
-    document.addEventListener('keydown', evt => {
-      this._handleEscClose(evt);
-    });
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_is-open');
-    document.removeEventListener('keydown', evt => {
-      this._handleEscClose(evt);
-    });
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   setEventListener() {
